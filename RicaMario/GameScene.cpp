eb54568a6e5bd6a6b1a8547efe4f053e4760f74d
Zak3-Kagu <kaz3@mbf.nifty.com>
2016-06-21 11:37:57 +0900

    copied from Kagutyo2013

--- RicaMario/GameScene.cpp
+++ RicaMario/GameScene.cpp
@@ -1,5 +1,13 @@
 #include "GameScene.h"
 
-scene::Game::Game() :now_state(){
+scene::Game::Game(){
+	now_state.reset(nullptr);
+}
 
+void scene::Game::update(std::unique_ptr<Base>& new_scene, const std::unique_ptr<Root>& root){
+	now_state->update(*this, now_state, root);
+	if (now_state == nullptr){
+		new_scene.reset(nullptr);
+		return;
+	}
 }
--- RicaMario/GameScene.h
+++ RicaMario/GameScene.h
@@ -22,13 +22,7 @@ namespace scene{
 
 	public:
 		Game();
-		void update(std::unique_ptr<Base>& new_scene, const std::unique_ptr<Root>& root)override{
-			now_state->update(*this,now_state,root);
-			if (now_state == nullptr){
-				new_scene.reset(nullptr);
-				return;
-			}
-		}
+		void update(std::unique_ptr<Base>& new_scene, const std::unique_ptr<Root>& root)override;
 	};
 }
 
